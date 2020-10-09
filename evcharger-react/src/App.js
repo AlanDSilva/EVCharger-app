96d19e561df73539321fd3a927563d12572b4f2a
Alan <dasilva.alan@outlook.com>
2020-10-09 15:20:37 +0300

    basic Change view

--- evcharger-react/src/App.js
+++ evcharger-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import StationLocator from "./components/StationLocator/StationLocator";
@@ -8,6 +8,27 @@ import Login from "./components/Login/Login";
 function App() {
   const [isAuthenticated, setAuthenticated] = useState(false);
   const [userInfo, setUserInfo] = useState(null);
+
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  const toggle = () => {
+    setIsActive(!isActive);
+  };
 
   const onLogin = (userInfo) => {
     setAuthenticated(true);
@@ -27,7 +48,14 @@ function App() {
         <Route path="/" exact component={StationLocator} />
         <Route
           path="/charge"
-          render={(routeProps) => <Charge isAuthenticated={isAuthenticated} />}
+          render={(routeProps) => (
+            <Charge
+              isAuthenticated={isAuthenticated}
+              isActive={isActive}
+              seconds={seconds}
+              toggle={toggle}
+            />
+          )}
         />
         <Route
           path="/login"
--- evcharger-react/src/components/Charge/Charge.js
+++ evcharger-react/src/components/Charge/Charge.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Modal from "../UI/Modal/Modal";
+import Input from "../UI/Input/Input";
+import Button from "../UI/Button/Button";
 
 const Charge = (props) => {
+  const { isActive, seconds, toggle } = props;
+
+  const [viewModal, setViewModal] = useState(false);
+  const cancelModalHandler = () => {
+    setViewModal(false);
+  };
+  const continueModalHandler = () => {
+    alert("You Continue!");
+  };
+
+  const submitCode = (event) => {
+    event.preventDefault();
+    if (!isActive) {
+      setViewModal(true);
+    }
+  };
+
+  let mins = Math.floor(seconds / 60);
+
   return (
     <div>
-      <h1>Protected</h1>
-      {props.isAuthenticated ? (
-        <p>User authenticated</p>
-      ) : (
-        <p>Sorry you need to login first</p>
-      )}
+      <Modal show={viewModal} modalClosed={cancelModalHandler}>
+        <h2>Confirm Order</h2>
+        <h3>Total: {(mins + 1) * 0.2}€</h3>
+        <Button btnType="danger" clicked={cancelModalHandler}>
+          Continue charging
+        </Button>
+        <Button btnType="success" clicked={continueModalHandler}>
+          Confirm
+        </Button>
+      </Modal>
+      <form onSubmit={submitCode}>
+        <Input
+          label="Charger Code"
+          inputtype="input"
+          placeholder="Enter code"
+        />
+        <Button clicked={toggle} btnType={isActive ? "danger" : "success"}>
+          {isActive ? "Stop Charging" : "Start Charging"}
+        </Button>
+      </form>
+
+      <p>
+        Timer {mins}:{seconds}
+      </p>
+      <p>Total: {(mins + 1) * 0.2}e</p>
     </div>
   );
 };
